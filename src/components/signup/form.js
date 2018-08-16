4dd1de9ee842cb044f8c764883d407b34b29cfc0
Antonio <toony1908@gmail.com>
2018-08-16 18:13:36 -0500

    adding basic styles

--- src/components/signup/form.js
+++ src/components/signup/form.js
@@ -2,45 +2,107 @@ import React, {Component} from 'react';
 import Card from '@material-ui/core/Card'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 class SignUpForm extends Component {
+
+    state = {
+        email: "",
+        name: "",
+        last_name: "",
+        password: "",
+        date: ""
+
+    }
+
+    handleEmail = (event) => {
+        console.log(this.state);
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handleName = (event) => {
+        console.log(this.state);
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleLastName = (event) => {
+        console.log(this.state);
+        this.setState({
+            last_name: event.target.value
+        })
+    }
+    
+    handlePassword = (event) => {
+        console.log(this.state);
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleDate = (event) => {
+        console.log(this.state);
+        this.setState({
+            date: event.target.value
+        })
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        const url = "http://localhost:3000/users/signup";
+        axios.post(url, {
+            user: this.state
+        }).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
     render () {
         return (
-            <Grid container justify="center" alignItems="center">
                 <div>
-                <Grid item xs={12}>
                     <Card>
                         Registrate en Airbnb Clone
-                        <Grid item xs={4}>
-                            <TextField />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField 
-                                type="date"
-                            />
-                        </Grid>
-                        <Grid item xs={4}>
+                        <TextField 
+                            id="email"
+                            value={this.state.email}
+                            onChange={this.handleEmail}
+
+                        />
+                        <TextField
+                            id="name"
+                            value={this.state.name} 
+                            onChange={this.handleName}
+                        />
+                        <TextField 
+                            id="last_name"
+                            value={this.state.last_name}
+                            onChange={this.handleLastName}
+                        />
+                        <TextField 
+                            id="password"
+                            value={this.state.password}
+                            onChange={this.handlePassword}
+                        />
+                        <TextField 
+                            id="date"
+                            type="date"
+                            value={this.state.date}
+                            onChange={this.handleDate}
+                        />
                         <Button 
                             variant="contained" 
                             color="secondary"
+                            onClick={this.handleClick}
                         >
-                        Registrate
+                            Registrate
                         </Button>
-                        </Grid>
                     </Card>
-                    </Grid>
                 </div>
-            </Grid>
         )
     }
 }
